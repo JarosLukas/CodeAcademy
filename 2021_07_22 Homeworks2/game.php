063a89e2bcf5b2b6bda301aa56dc7cd76a583bf8
Jaros <jaros.lukas777@gmail.com>
2021-07-25 19:15:00 +0100

    game

---
+++ 2021_07_22 Homeworks2/game.php
@@ -0,0 +1,55 @@
+<?php
+// Parašykite funkciją, kuri mestų kauliuką iki kol iškris 5 arba 6. Visus savo metimus išvestų į ekrana.(while).
+// https://www.codecademy.com/forum_questions/5160194aa66544e30e002454
+
+$rollCount = 0;
+do {
+    $roll = rand(1,6);
+    $rollCount ++;
+    if ($roll != 6){
+        echo '<p> Išriedėjo ' . $roll . '</p>';
+    }
+    else {
+        echo 'Pagaliau iškrito ' . $roll ;
+    }
+} while ($roll != 6);
+$verb = 'Bandymų';
+$last = 'škaičius';
+
+echo "<p>{$verb} {$last} {$rollCount}!</p>";
+echo '<hr>';
+
+// Papildomas funkcionalumas žaidimui(papildomas sudėtingumas):
+// Metame 2 kauliukus (arba vieną kauliuką 2 kartus).
+	// taisyklės:
+	// Jeigu kauliukų suma yra mažiau už 5 reiškia - jūs iškarto pralaimejote.
+	// Jeigu kauliukų suma daugiau už 5 ir:
+	// Išmesta buvo 6 ir 6, reiškia - laimėjote dviratį.
+	// Abiejų kauliukų išmestas skaičius yra vienodas, bet ne 6 ir 6 - reiškia jūs laimėjote bilietą į kiną.
+	// Kitais atvejais – kauliukas metamas dar kartą.
+	
+// Žaidimas turi vykti iki kol žaidėjas laimės / pralaimės(while)
+	
+// Visa žaidimo eiga turėtų būti išvesta į ekraną.
+
+
+function rollDice()
+{
+        $dice1roll = rand(1, 6);
+        $dice2roll = rand(1, 6);
+        $sum = $dice1roll + $dice2roll;
+
+        if ($sum > 5 && $dice1roll === $dice2roll && $dice1roll !== 6 && $dice2roll !==6) {
+            return 'Jūs laimėjote BILIETĄ Į KINĄ! Išriedėjo ' .$dice1roll .' ir ' .$dice2roll;
+        }
+        if ($dice1roll === 6 && $dice2roll === 6) {
+            return 'Laimėjote DVIRATĮ! Išriedėjo ' .$dice1roll .' ir ' .$dice2roll;
+        }
+        if ($sum > 5 && $sum <= 11) {
+            return 'Bandykite dar kartą! Išriedėjo ' .$dice1roll .' ir ' .$dice2roll;
+        }
+        if ($sum <= 5) {
+            return 'PRALAIMĖJOTE! Išriedėjo ' .$dice1roll .' ir ' .$dice2roll;
+        }
+}
+var_dump(rollDice());
