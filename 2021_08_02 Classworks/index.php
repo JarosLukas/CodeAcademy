577adcad2ad374758bea023bce404aecca9b77f9
Jaros <jaros.lukas777@gmail.com>
2021-08-05 18:49:32 +0100

    PHP OOP

--- 2021_08_02 Classworks/index.php
+++ 2021_08_02 Classworks/index.php
@@ -2,12 +2,6 @@
 
 require_once 'user.php';
 
-// $user = [
-//     'name'    => 'Jaroslav',
-//     'surname' => 'Lukasevic',
-//     'age'     => '65',
-//     'mail'    => 'jaroslav.lukasevic@gmail.com'
-// ];
 
 $user = new User ('Juozas','Povilaitis','65','Juozas.Povilaitis@gmail.com');
 
